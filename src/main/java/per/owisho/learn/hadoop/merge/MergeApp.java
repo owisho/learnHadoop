8a99aadddc37c1d94fd066bc01c259f714106930
wangyang <yangw@opera.com>
2020-04-16 19:30:55 +0800

    add merge test app

---
+++ src/main/java/per/owisho/learn/hadoop/merge/MergeApp.java
@@ -0,0 +1,58 @@
+package per.owisho.learn.hadoop.merge;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class MergeApp {
+
+    static class SequenceFileMapper extends Mapper<NullWritable, BytesWritable, Text, BytesWritable> {
+        private Text filenameKey;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            InputSplit split = context.getInputSplit();
+            Path path = ((FileSplit) split).getPath();
+            filenameKey = new Text(path.toString());
+        }
+
+        @Override
+        protected void map(NullWritable key, BytesWritable value, Context context) throws IOException, InterruptedException {
+            context.write(filenameKey, value);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String INPUT_PATH = "hdfs://hadoop000:8020/inputmerge";
+        String OUTPUT_PATH = "hdfs://hadoop000:8020/outputmerge";
+        Configuration conf = new Configuration();
+        final FileSystem fileSystem = FileSystem.get(new URI(INPUT_PATH), conf);
+        if (fileSystem.exists(new Path(OUTPUT_PATH))) {
+            fileSystem.delete(new Path(OUTPUT_PATH), true);
+        }
+        Job job = Job.getInstance(conf, "MergeApp");
+        job.setJarByClass(MergeApp.class);
+        job.setInputFormatClass(WholeFileInputFormat.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(BytesWritable.class);
+        job.setMapperClass(SequenceFileMapper.class);
+        FileInputFormat.addInputPath(job, new Path(INPUT_PATH));
+        FileOutputFormat.setOutputPath(job, new Path(OUTPUT_PATH));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
