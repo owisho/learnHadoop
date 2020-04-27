c0990355aa91a372d68fe9deb7dfba851bad9832
owisho <owisho2012@126.com>
2020-04-27 15:44:23 +0800

    fix bug

--- src/main/java/per/owisho/learn/hadoop/merge/WholeFileRecordReader.java
+++ src/main/java/per/owisho/learn/hadoop/merge/WholeFileRecordReader.java
@@ -7,10 +7,10 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 import java.io.IOException;
 
