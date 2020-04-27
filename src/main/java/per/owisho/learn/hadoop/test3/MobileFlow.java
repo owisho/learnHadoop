9aaf77fe633947abf37ad429fd4b131831364352
wangyang <yangw@opera.com>
2020-04-27 17:04:39 +0800

    add my mobile info test class

---
+++ src/main/java/per/owisho/learn/hadoop/test3/MobileFlow.java
@@ -0,0 +1,95 @@
+package per.owisho.learn.hadoop.test3;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class MobileFlow implements WritableComparable<MobileFlow> {
+
+    private String msisdn;
+    private Long upPackNum;
+    private Long downPackNum;
+    private Long upPayLoad;
+    private Long downPayLoad;
+
+    public MobileFlow(String msisdn, Long upPackNum, Long downPackNum, Long upPayLoad, Long downPayLoad) {
+        this.msisdn = msisdn;
+        this.upPackNum = upPackNum;
+        this.downPackNum = downPackNum;
+        this.upPayLoad = upPayLoad;
+        this.downPayLoad = downPayLoad;
+    }
+
+    public MobileFlow(MobileFlow info){
+        this.msisdn = info.msisdn;
+        this.upPackNum = info.upPackNum;
+        this.downPackNum = info.downPackNum;
+        this.upPayLoad = info.upPayLoad;
+        this.downPayLoad = info.downPayLoad;
+    }
+
+    public String getMsisdn() {
+        return msisdn;
+    }
+
+    public void setMsisdn(String msisdn) {
+        this.msisdn = msisdn;
+    }
+
+    public Long getUpPackNum() {
+        return upPackNum;
+    }
+
+    public void setUpPackNum(Long upPackNum) {
+        this.upPackNum = upPackNum;
+    }
+
+    public Long getDownPackNum() {
+        return downPackNum;
+    }
+
+    public void setDownPackNum(Long downPackNum) {
+        this.downPackNum = downPackNum;
+    }
+
+    public Long getUpPayLoad() {
+        return upPayLoad;
+    }
+
+    public void setUpPayLoad(Long upPayLoad) {
+        this.upPayLoad = upPayLoad;
+    }
+
+    public Long getDownPayLoad() {
+        return downPayLoad;
+    }
+
+    public void setDownPayLoad(Long downPayLoad) {
+        this.downPayLoad = downPayLoad;
+    }
+
+    @Override
+    public int compareTo(MobileFlow o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeUTF(msisdn);
+        out.writeLong(upPackNum);
+        out.writeLong(downPackNum);
+        out.writeLong(upPackNum);
+        out.writeLong(downPackNum);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        this.msisdn = in.readUTF();
+        this.upPackNum = in.readLong();
+        this.downPackNum = in.readLong();
+        this.upPayLoad = in.readLong();
+        this.downPayLoad = in.readLong();
+    }
+}
---
+++ src/main/java/per/owisho/learn/hadoop/test3/MobileFlowApp.java
@@ -0,0 +1,96 @@
+package per.owisho.learn.hadoop.test3;
+
+import com.google.common.base.Splitter;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Iterator;
+
+public class MobileFlowApp {
+
+    static Splitter sp = Splitter.on("\t").trimResults();
+
+    static class MyMapper extends Mapper<IntWritable, Text, Text, MobileFlow> {
+        @Override
+        protected void map(IntWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String content = value.toString();
+            //TODO 搞懂splitter源码
+            Iterable<String> itr = sp.split(content);
+
+            String[] arr = content.split("\t");
+            String msisdn = arr[1];
+            String upPackNumStr = arr[6];
+            String downPackNumStr = arr[7];
+            String upPayLoadStr = arr[8];
+            String downPayLoadStr = arr[9];
+
+            Long upPackNum = Long.parseLong(upPackNumStr);
+            Long downPackNum = Long.parseLong(downPackNumStr);
+            Long upPayLoad = Long.parseLong(upPayLoadStr);
+            Long downPayLoad = Long.parseLong(downPayLoadStr);
+            MobileFlow flow = new MobileFlow(msisdn, upPackNum, downPackNum, upPayLoad, downPayLoad);
+            context.write(new Text(msisdn), flow);
+        }
+    }
+
+    static class MyReducer extends Reducer<Text, MobileFlow, NullWritable, MobileFlow> {
+        @Override
+        protected void reduce(Text key, Iterable<MobileFlow> values, Context context) throws IOException, InterruptedException {
+            MobileFlow result = null;
+            Iterator<MobileFlow> itr = values.iterator();
+            while (itr.hasNext()) {
+                MobileFlow next = itr.next();
+                if (result == null) {
+                    result = new MobileFlow(next);
+                } else {
+                    result.setUpPackNum(result.getUpPackNum() + next.getUpPackNum());
+                    result.setDownPackNum(result.getDownPackNum() + next.getDownPackNum());
+                    result.setUpPayLoad(result.getUpPayLoad() + next.getUpPayLoad());
+                    result.setDownPayLoad(result.getDownPayLoad() + next.getDownPayLoad());
+                }
+            }
+
+            context.write(NullWritable.get(), result);
+        }
+    }
+
+    public static void main(String[] args) throws URISyntaxException, IOException, ClassNotFoundException, InterruptedException {
+        String INPUT_FILE = "hdfs://hadoop000:8020/inputMobileTestInfo";
+        String OUTPUT_FILE = "hdfs://hadoop000:8020/outputMobileTestInfo";
+
+        Configuration conf = new Configuration();
+        FileSystem fileSystem = FileSystem.get(new URI(INPUT_FILE),conf);
+        if(fileSystem.exists(new Path(OUTPUT_FILE))){
+            fileSystem.delete(new Path(OUTPUT_FILE),true);
+        }
+
+        Job job = Job.getInstance(conf, "Mobile Info Test");
+        job.setJarByClass(MobileFlow.class);
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReducer.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(MobileFlow.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(MobileFlow.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(OUTPUT_FILE));
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+    }
+
+}
